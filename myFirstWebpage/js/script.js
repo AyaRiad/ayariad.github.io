24bb423a73a282297ad8a9b0dae8f9fa794e0d26
AyaRiad <66526040+AyaRiad@users.noreply.github.com>
2020-06-08 01:41:12 +0400

    Test

---
+++ myFirstWebpage/js/script.js
@@ -0,0 +1,5 @@
+
+
+function changeImage(){  //This is an action that will happen when an event happens
+	document.getElementById("pugImages").src = "images/pug 2.jpg"; // Change the source of the image to a new source
+}
